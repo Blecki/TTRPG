739bb38b665127f0844861bf6d3a8dbf4211e087
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 14:56:28 -0400

    Create Savant.js

---
+++ features/Savant.js
@@ -0,0 +1,10 @@
+{ 
+  NAME: "SAVANT", 
+  DESCRIPTION: "Your proficiency bonus increases by 1.",
+  SEQUENCE: 0,
+  REQUIREMENTS: "LEVEL 8, TALENTED",
+  AVAILABLE: (f, c) => c.LEVEL >= 8 && hasFeature('TALENTED'),
+  APPLY_STATS: (f, c) => {},
+  APPLY_FEATURE: (f, c) => { c.PROFICIENCY_BONUS += 1; },
+  TAGS: [ "PROFICIENCY", "COMMON" ]
+}
