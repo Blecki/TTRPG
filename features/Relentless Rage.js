befdfc66796335c3c57843beb90316ee80210eeb
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 15:01:36 -0400

    Create Relentless Rage.js

---
+++ features/Relentless Rage.js
@@ -0,0 +1,10 @@
+{ 
+  NAME: "RELENTLESS RAGE", 
+  DESCRIPTION: "",
+  SEQUENCE: 2,
+  REQUIREMENTS: "LEVEL 11, RAGE",
+  AVAILABLE: (f, c) => c.LEVEL >= 11 && hasFeature("RAGE"),
+  APPLY_STATS: (f, c) => {},
+  APPLY_FEATURE: (f, c) => { c.ADDITIONAL_FEATURES.push(f); },
+  TAGS: [ "BARBARIAN" ]
+}
