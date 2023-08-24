2761b58a399ee324c022f1a863415680f7b436ab
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 15:00:07 -0400

    Create Reckless Attack.js

---
+++ features/Reckless Attack.js
@@ -0,0 +1,10 @@
+{ 
+  NAME: "RECKLESS ATTACK", 
+  DESCRIPTION: "",
+  SEQUENCE: 2,
+  REQUIREMENTS: "LEVEL 2",
+  AVAILABLE: (f, c) => c.LEVEL >= 2,
+  APPLY_STATS: (f, c) => {},
+  APPLY_FEATURE: (f, c) => { c.ADDITIONAL_FEATURES.push(f); },
+  TAGS: [ "BARBARIAN" ]
+}
