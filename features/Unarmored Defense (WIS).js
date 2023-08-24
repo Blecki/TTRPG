c34ac3127336ab4cbc006a9d777f34f05ff82b53
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 14:54:19 -0400

    Create Unarmored Defense (WIS).js

---
+++ features/Unarmored Defense (WIS).js
@@ -0,0 +1,10 @@
+{ 
+  NAME: "Unarmored Defense (WIS)", 
+  DESCRIPTION: "When not wearing armor, add your WIS modifier to AC",
+  SEQUENCE: 1,
+  REQUIREMENTS: "WIS > 14, Can't have Unarmored Defense (CON)",
+  AVAILABLE: (f, c) => c.STATS.WIS > 14 && !hasFeature("Unarmored Defense (CON)"),
+  APPLY_STATS: (f, c) => {},
+  APPLY_FEATURE: (f, c) => { c.ARMOR_CLASS += c.MODIFIERS.WIS; },
+  TAGS: [ "COMMON", "ARMOR CLASS", "MONK" ]
+}
