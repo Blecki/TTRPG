3da189f00c74d11af64e929640786c9fa9ec3dd3
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 20:23:28 -0400

    Update Danger Sense.js

--- features/Danger Sense.js
+++ features/Danger Sense.js
@@ -1,6 +1,6 @@
 { 
   NAME: "DANGER SENSE", 
-  DESCRIPTION: "",
+  DESCRIPTION: "You have an uncanny sense for when things are about to get hairy. You have advantage on Dexterity saving throws against effects that you can see, such as traps and spells.",
   SEQUENCE: 2,
   REQUIREMENTS: "LEVEL 2",
   AVAILABLE: (f, c) => c.LEVEL >= 2,
