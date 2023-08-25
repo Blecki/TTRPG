7a09710b13d424ecdda3a550715bbf19713d0022
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 20:22:22 -0400

    Update Reckless Attack.js

--- features/Reckless Attack.js
+++ features/Reckless Attack.js
@@ -1,6 +1,7 @@
 { 
   NAME: "RECKLESS ATTACK", 
-  DESCRIPTION: "",
+  DESCRIPTION: "When you make your first attack on your turn, you can decide to attack recklessly. Doing so gives you advantage on melee weapon attack rolls using Strength during this turn, but attack rolls against you have advantage until your next turn.",
+  COST: "1 FP",
   SEQUENCE: 2,
   REQUIREMENTS: "LEVEL 2",
   AVAILABLE: (f, c) => c.LEVEL >= 2,
