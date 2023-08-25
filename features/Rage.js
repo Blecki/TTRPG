878bc7d70c703e6928943d01380ecebc93d27ad4
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 20:19:40 -0400

    Update Rage.js

--- features/Rage.js
+++ features/Rage.js
@@ -1,7 +1,8 @@
 { 
   NAME: "RAGE", 
-  DESCRIPTION: "Spend 1 feature point to enter a rage. While raging, you have advantage on strength based saving throws and checks, you gain resistence to bludgeoning, piercing, and slashing damage, and you can add your proficiency bonus to the damage of melee attacks.",
+  DESCRIPTION: "Use your bonus action to enter a rage. While raging, you have advantage on strength based saving throws and checks, you gain resistence to bludgeoning, piercing, and slashing damage, and you can add your proficiency bonus to the damage of melee attacks.",
   SEQUENCE: 2,
+  COST: "1 FP",
   REQUIREMENTS: "",
   AVAILABLE: (f, c) => true,
   APPLY_STATS: (f, c) => {},
