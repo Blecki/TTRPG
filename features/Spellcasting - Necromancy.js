4aa207095305b11a52a67de8fe51962ac7d4d038
Blecki <1297068+Blecki@users.noreply.github.com>
2023-08-24 21:01:38 -0400

    Create Spellcasting - Necromancy.js

---
+++ features/Spellcasting - Necromancy.js
@@ -0,0 +1,10 @@
+{ 
+  NAME: "SPELLCASTING: NECROMANCY", 
+  DESCRIPTION: "You are able to learn and cast spells of the school of necromancy.",
+  SEQUENCE: 1,
+  REQUIREMENTS: "INT > 14",
+  AVAILABLE: (f, c) => c.STATS.INT > 14,
+  APPLY_STATS: (f, c) => {},
+  APPLY_FEATURE: (f, c) => { c.ADDITIONAL_FEATURES.push(f); },
+  TAGS: [ "SPELLCASTING", "NECROMANCY" ]
+}
